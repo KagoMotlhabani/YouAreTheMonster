92a4fe20485cc0fe6aa8099dc03969aada14df39
Kago Motlhabani <kagomw@gmail.com>
2025-11-06 07:12:43 +0200

    Added direction flip

--- YouAreTheMonster/Assets/Scripts/PlayerMovement.cs
+++ YouAreTheMonster/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 5;
+    public float horzDirection; //used to check which direction player is facing
     public bool isGrounded;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        horzDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horzDirection * speed, rb.velocity.y);
+
+        //facing left and right logic
+        if (horzDirection >= 0.01f)
+        {
+            transform.eulerAngles = new Vector3 (0,0,0); //facing right
+        }
+        if(horzDirection <= -0.01f)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0); //facing left
+        }
 
         //jumping
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, speed);
+            rb.velocity = new UnityEngine.Vector2(rb.velocity.x, speed);
             isGrounded = false;
         }
 
